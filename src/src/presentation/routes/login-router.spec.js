a575f994fac489ad4af32c60e832a74022d4d038
Gemirson <gemirson.silva@gmail.com>
2020-07-13 17:56:19 -0300

    ensure LoginRouter calls EmailValidator with correct email

--- src/src/presentation/routes/login-router.spec.js
+++ src/src/presentation/routes/login-router.spec.js
@@ -18,6 +18,7 @@ const makeSut = () => {
 const makeEmailValidator = () => {
   class EmailValidatorSpy {
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -229,5 +230,17 @@ describe('Login Router', () => {
     const httpResponse = await sut.router(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+    await sut.router(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })

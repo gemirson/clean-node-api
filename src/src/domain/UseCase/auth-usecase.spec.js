d8601c38a96ee196060178e8cd202499a2984ce8
Gemirson <gemirson.silva@gmail.com>
2020-07-14 16:10:40 -0300

    move sut creating to a factory method

--- src/src/domain/UseCase/auth-usecase.spec.js
+++ src/src/domain/UseCase/auth-usecase.spec.js
@@ -14,27 +14,36 @@ class AuthUsecase {
     await this.loadUseByEmailRepositorySpy.load(email)
   }
 }
+
+const makeSut = () => {
+  class LoadUseByEmailRepositorySpy {
+    async load (email) {
+      this.email = email
+    }
+  }
+  const loadUseByEmailRepositorySpy = new LoadUseByEmailRepositorySpy()
+  const sut = new AuthUsecase(loadUseByEmailRepositorySpy)
+  return {
+    sut,
+    loadUseByEmailRepositorySpy
+  }
+}
+
 describe('Auth UseCase', () => {
   test('Should return throw if no email is provided', async () => {
-    const sut = new AuthUsecase()
+    const { sut } = makeSut()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('Should return throw if no password is provided', async () => {
-    const sut = new AuthUsecase()
+    const { sut } = makeSut()
     const promise = sut.auth('any_email@mail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 
   test('Should call LoadUseByEmailRepository with correct email', async () => {
-    class LoadUseByEmailRepositorySpy {
-      async load (email) {
-        this.email = email
-      }
-    }
-    const loadUseByEmailRepositorySpy = new LoadUseByEmailRepositorySpy()
-    const sut = new AuthUsecase(loadUseByEmailRepositorySpy)
+    const { sut, loadUseByEmailRepositorySpy } = makeSut()
     await sut.auth('any_email@mail.com', 'any_password')
     expect(loadUseByEmailRepositorySpy.email).toBe('any_email@mail.com')
   })

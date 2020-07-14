a0b9d26040b7e39f4cab201545c09c1bc116b193
Gemirson <gemirson.silva@gmail.com>
2020-07-14 20:30:17 -0300

    simplify code

--- src/src/domain/UseCase/auth-usecase.js
+++ src/src/domain/UseCase/auth-usecase.js
@@ -15,14 +15,12 @@ module.exports = class AuthUsecase {
       throw new MissingParamError('password')
     }
     const user = await this.loadUseByEmailRepository.load(email)
-    if (!user) {
-      return null
+    const isValid = user && await this.encrypter.compare(password, user.password)
+    if (isValid) {
+      const acessToken = await this.tokenGeneratorSpy.generate(user.id)
+      return acessToken
     }
-    const isValid = await this.encrypter.compare(password, user.password)
-    if (!isValid) {
-      return null
-    }
-    const acessToken = await this.tokenGeneratorSpy.generate(user.id)
-    return acessToken
+
+    return null
   }
 }

5e3ece1215f769932b3403762ed955c9d6f9fa01
Gemirson <gemirson.silva@gmail.com>
2020-07-13 22:18:17 -0300

    move sut creating to factory helpded method

--- src/src/domain/validate/email-validator.spec.js
+++ src/src/domain/validate/email-validator.spec.js
@@ -5,16 +5,20 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Shoul return true if validator true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValidator = sut.isValid('valid_email@mail.com')
     expect(isEmailValidator).toBe(true)
   })
 
   test('Shoul return false if validator false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValidator = sut.isValid('invalid_email@mail.com')
     expect(isEmailValidator).toBe(false)
   })

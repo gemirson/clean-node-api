73e83a72dbe94f32471fb07c337e1472761ad214
Gemirson <gemirson.silva@gmail.com>
2020-07-14 23:38:40 -0300

    ensure EmailValidator throw if no email is provided

--- src/src/domain/validate/email-validator.js
+++ src/src/domain/validate/email-validator.js
@@ -1,7 +1,11 @@
 const validator = require('validator')
+const { MissingParamError } = require('../../utils/erros')
 
 module.exports = class EmailValidator {
   isValid (email) {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
     return validator.isEmail(email)
   }
 }

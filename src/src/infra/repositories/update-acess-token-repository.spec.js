efd1410dbb059e87911465dbb8a656931744e885
Gemirson <gemirson.silva@gmail.com>
2020-07-16 15:32:59 -0300

    ensure UpdateAcessTokenRepository update the user with given acessToken

---
+++ src/src/infra/repositories/update-acess-token-repository.spec.js
@@ -0,0 +1,43 @@
+const MongoHelper = require('../../infra/helpers/mongo-helper')
+let db
+class UpdateAcessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async update (userId, acessToken) {
+    await this.userModel.updateOne({ _id: userId }
+      , {
+        $set: {
+          acessToken
+        }
+      })
+  }
+}
+describe('Auth UseCase', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    db = await MongoHelper.getDb()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should update the user with given acessToken', async () => {
+    const userModel = db.collection('users')
+    const sut = new UpdateAcessTokenRepository(userModel)
+    const fakeUser = await userModel.insertOne({
+      email: 'valid_email@mail.com',
+      password: 'hashed_password',
+      name: 'any_name'
+    })
+    await sut.update(fakeUser.ops[0]._id, 'valid_token')
+    const updateFakeUser = await userModel.findOne({ _id: fakeUser.ops[0]._id })
+    expect(updateFakeUser.acessToken).toBe('valid_token')
+  })
+})

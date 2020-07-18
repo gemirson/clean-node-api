a0244b848b6373ce5a0547c739b43cbec1eb2fe8
Gemirson <gemirson.silva@gmail.com>
2020-07-17 22:58:40 -0300

    Merge
    Merge

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxxx/clan-node-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxxx/clan-node-api',
+  tokenSecret:process.env.TOKEN_SECRET || 'secret'
 }

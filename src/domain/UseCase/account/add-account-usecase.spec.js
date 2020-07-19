7cd1fb6966f47b52eb4f589ef852c5799e508286
Gemirson <gemirson.silva@gmail.com>
2020-07-19 14:41:51 -0300

    ensure AddAccountUseCase return throw if no password is provided

--- src/domain/UseCase/account/add-account-usecase.spec.js
+++ src/domain/UseCase/account/add-account-usecase.spec.js
@@ -1,9 +1,13 @@
 const { MissingParamError } = require('../../../utils/erros')
 
 class AddAccountUseCase {
-  async add (email) {
+  async add (email, password) {
     if (!email) {
       throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
     }
 
     return true
@@ -22,5 +26,10 @@ describe('AddAccount ', () => {
     const { sut } = makeSut()
     const promise = sut.add()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+  test('Should return throw if no email is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.add('any_email@gmail.com')
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })

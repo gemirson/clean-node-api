6d5ae15847dd4798488073946aba82b2b1fa82ca
Gemirson <gemirson.silva@gmail.com>
2020-07-20 20:25:58 -0300

    ensure MongoHelper creating save  method

--- src/infra/helpers/mongo-helper.js
+++ src/infra/helpers/mongo-helper.js
@@ -15,12 +15,26 @@ module.exports = {
     this.db = null
   },
   async getCollection (name) {
+    this.namedb = name
     if (!this.client || !this.client.isConnected()) {
       await this.connect(this.uri)
     }
     return this.db.collection(name)
   },
   async save (email, haspassword, name) {
+    const userModel = this.db.collection(this.namedb)
+    const newUser = await userModel.insertOne({
+      email: email,
+      password: haspassword,
+      name: name
+    })
+    if (newUser) {
+      return {
+        _id: newUser.ops[0]._id,
+        name: newUser.ops[0].name,
+        email: newUser.ops[0].email
+      }
+    }
     return null
   }
 

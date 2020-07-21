12b072a639b617aeaa3dfd7a86b68e845ac569f1
Gemirson <gemirson.silva@gmail.com>
2020-07-20 22:45:02 -0300

    ensure Add Account User Route return 400 if no name is proveded

--- src/presentation/routes/add-account-user-router.spec.js
+++ src/presentation/routes/add-account-user-router.spec.js
@@ -3,6 +3,9 @@ const HttpResponse = require('../helpers/http-response')
 
 class AddAccountRoute {
   async route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
+    }
     const { email, password, name } = httpRequest.body
     if (!email) {
       return HttpResponse.badRequest(new MissingParamError('email'))
@@ -21,6 +24,18 @@ describe('Add Account User Route', () => {
     const sut = new AddAccountRoute()
     const httpRequest = {
       body: {
+        email: 'any_email'
+      }
+    }
+
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.error).toBe(new MissingParamError('password').message)
+  })
+  test('Should return 400 if no email is proveded', async () => {
+    const sut = new AddAccountRoute()
+    const httpRequest = {
+      body: {
         password: 'any_password'
       }
     }
@@ -28,5 +43,17 @@ describe('Add Account User Route', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body.error).toBe(new MissingParamError('email').message)
+  })
+  test('Should return 400 if no name is proveded', async () => {
+    const sut = new AddAccountRoute()
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.error).toBe(new MissingParamError('name').message)
   })
 })

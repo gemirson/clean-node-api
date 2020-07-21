9e2ab32f828d035a05e6482ff7821d42b8b9037f
Gemirson <gemirson.silva@gmail.com>
2020-07-20 23:26:00 -0300

    ensure AddAccountRouter call AddAccountUseCase with correct params

--- src/presentation/routes/add-account-user-router.spec.js
+++ src/presentation/routes/add-account-user-router.spec.js
@@ -3,8 +3,11 @@ const HttpResponse = require('../helpers/http-response')
 const { ServerError } = require('../erros')
 
 class AddAccountUseCaseSpy {
-  async save () {
-    return true
+  async save (email, password, name) {
+    this.email = email
+    this.password = password
+    this.name = name
+    return this.user
   }
 }
 class AddAccountRoute {
@@ -26,6 +29,8 @@ class AddAccountRoute {
     if (!name) {
       return HttpResponse.badRequest(new MissingParamError('name'))
     }
+    this.user = await this.addAccountUseCase.save(email, password, name)
+    return this.user
   }
 }
 
@@ -34,7 +39,8 @@ const makeSut = () => {
   const addAccountRoute = new AddAccountRoute(addAccountUseCaseSpy)
 
   return {
-    sut: addAccountRoute
+    sut: addAccountRoute,
+    addAccountUseCaseSpy
   }
 }
 
@@ -93,5 +99,19 @@ describe('Add Account User Route', () => {
     const httpResponse = await sut.route()
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body.error).toBe(new ServerError().message)
+  })
+  test('Should call AddAccountUseCase with correct params', async () => {
+    const { sut, addAccountUseCaseSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        name: 'any_name'
+      }
+    }
+    await sut.route(httpRequest)
+    expect(addAccountUseCaseSpy.email).toBe(httpRequest.body.email)
+    expect(addAccountUseCaseSpy.password).toBe(httpRequest.body.password)
+    expect(addAccountUseCaseSpy.name).toBe(httpRequest.body.name)
   })
 })
